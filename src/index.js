0cc4f29a3dbc73434a50b0a685a41c3540624d79
tehpsalmist <bstewardcodes@gmail.com>
2020-03-20 23:35:28 -0400

    chore: update dependencies and improve demo usage

--- src/index.js
+++ src/index.js
@@ -1,33 +1,29 @@
 import React, { useState, useEffect } from 'react'
 import { render } from 'react-dom'
-import { znContext } from '@zenginehq/zengine-sdk'
+import { useZengineContext, ZnContextProvider } from '@zenginehq/react-sdk'
 
 export const App = props => {
-  const [context, setContext] = useState()
+  const { context, triggerContextRefresh } = useZengineContext()
   const [show, setShow] = useState(false)
 
   useEffect(() => {
-    if (!context) {
-      znContext((err, contextData) => {
-        if (err) {
-          console.error(err)
-        }
-
-        setContext(contextData)
-      })
-    }
-  }, [])
+    // logs context whenever it updates
+    console.log(context)
+  }, [context])
 
   return <main>
     <h1 style={{ textAlign: 'center' }}>Hello Zengine!</h1>
-    <button
-      onClick={e => setShow(!show)}
-      disabled={!context}
-    >
+    <button onClick={e => triggerContextRefresh()}>Refresh Context</button>
+    <button onClick={e => setShow(!show)}>
       {show ? 'Hide' : 'Show'} Context Data
     </button>
     {show && <pre>{JSON.stringify(context, null, 2)}</pre>}
   </main>
 }
 
-render(<App />, document.getElementById('app'))
+render(
+  <ZnContextProvider>
+    <App />
+  </ZnContextProvider>,
+  document.getElementById('app')
+)
