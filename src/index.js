fe4e9a734c8c0d8421fe2178cbf9dc4cbcb2fe86
Anna Parks <anna@wizehive.com>
2020-04-07 14:38:03 -0600

    feat: demo UI improvements

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from 'react'
 import { render } from 'react-dom'
 import { useZengineContext, ZnContextProvider } from '@zenginehq/react-sdk'
+import { znMessage, znPluginData } from '@zenginehq/zengine-sdk'
+import '@zenginehq/zengine-ui/style.css';
+import { Button } from '@zenginehq/zengine-ui-react';
 
 export const App = props => {
   const { context, triggerContextRefresh } = useZengineContext()
@@ -11,12 +14,41 @@ export const App = props => {
     console.log(context)
   }, [context])
 
+	const getBackend = () => {
+		znPluginData({
+			namespace: context.plugin.namespace,
+			method: 'get',
+			route: '/testabc',
+			options: {
+				params: {
+					id: context.workspace.forms[0].id
+				}
+			}
+		}).then(() => {
+			znMessage(`Backend Service Success`, 'saved');
+		}).catch(() => {
+			znMessage(`Backend Service Fail`, 'error');
+		})
+	}
+
   return <main>
     <h1 style={{ textAlign: 'center' }}>Hello Zengine!</h1>
-    <button onClick={e => triggerContextRefresh()}>Refresh Context</button>
-    <button onClick={e => setShow(!show)}>
-      {show ? 'Hide' : 'Show'} Context Data
-    </button>
+    <h1 style={{ textAlign: 'center' }}>Hello Zengine!</h1>
+    <ul className="list-inline">
+    <li className="list-inline-item">
+      <Button theme="secondary" onClick={e => triggerContextRefresh()}>Refresh Context</Button>
+    </li>
+    <li className="list-inline-item">
+      <Button theme="primary" onClick={e => setShow(!show)}>
+        {show ? 'Hide' : 'Show'} Context Data
+      </Button>
+    </li>
+    <li className="list-inline-item">
+      <Button theme="success" onClick={e => getBackend()}>
+        Query Backend
+      </Button>
+    </li>
+    </ul>
     {show && <pre>{JSON.stringify(context, null, 2)}</pre>}
   </main>
 }
